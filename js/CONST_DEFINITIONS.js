830d7533dc5df9bb0722da309fc124338d4c3fdc
Judy Barbieri <oliver.f.p.hesse@gmail.com>
2025-02-27 14:44:25 -0500

    working on page initialising

---
+++ js/CONST_DEFINITIONS.js
@@ -0,0 +1,9 @@
+const API_KEY = "AIzaSyCLGzd-Qm_toEeVi4j-lbhP99hY29ODOs4";
+const FOLDER_ID = '14BNBG1FBrAhz9r95GnrA_uoxLGFdv4FO';
+const DRIVE_LINK = "https://drive.google.com/drive/u/0/folders/14BNBG1FBrAhz9r95GnrA_uoxLGFdv4FO"
+const TestFileID = "11Q5PMbXKAvW3RX9kD67dyYTOMFIS6KD6"
+const FileURL = "https://drive.google.com/thumbnail?id="
+const FileExportData = "&export&sz=w1000"
+const config_url = "https://oliverhesse.github.io/Photo-Catalog/config.json"
+
+CONFIG_DETAILS = {}
--- js/Initialization.js
+++ js/Initialization.js
@@ -1,19 +1,35 @@
-const config_url = "https://oliverhesse.github.io/Photo-Catalog/config.json"
 const INIT_FUNCTION_CALL_ORDER = [load_config,get_initial_images]
-CONFIG_DETAILS = {}
 
 
-async function get_initial_images(){
-    const response = await fetch(`https://www.googleapis.com/drive/v3/files?pageSize=${CONFIG_DETAILS["initial-image-loaded-number"]}&q='${FOLDER_ID}'+in+parents&key=${API_KEY}`)
-    const data = await response.json();
-    console.log(data.files)
+//by default does not support nested folders
+async function load_image(image_data) {
+    let image_url = FileURL+image_data.id+FileExportData
+    let NewImage = new Image()
+    NewImage.src = image_url
+    let origin_height = NewImage.naturalHeight;
+    let origin_width = NewImage.naturalWidth
+    NewImage.height = CONFIG_DETAILS["img-height-default"]
+    NewImage.width = (origin_width)*(CONFIG_DETAILS["img-height-default"]/origin_height)
+    document.getElementsByClassName("img-container-holder")[0].appendChild(NewImage)
+    
+    console.log("image_loaded")
 }
 
+async function get_initial_images(){
+    console.log("Loading Images")
+    const response = await fetch(`https://www.googleapis.com/drive/v3/files?pageSize=${CONFIG_DETAILS["initial-image-loaded-number"]}&q='${FOLDER_ID}'+in+parents&key=${API_KEY}`)
+    const data = await response.json();
+
+    for(const image of data.files){
+        load_image(image)
+    }
+}
 
 async function load_config(){
     response = await fetch(config_url)
     data = await response.json()
     CONFIG_DETAILS = data
+    console.log(data)
     console.log("finished loading config")
  
 }
