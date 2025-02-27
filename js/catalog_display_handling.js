3d2dd5302154c0cdc117de3de5ae4c431b6a45c8
Judy Barbieri <oliver.f.p.hesse@gmail.com>
2025-02-27 13:50:42 -0500

    testing more fetch stuff

--- js/catalog_display_handling.js
+++ js/catalog_display_handling.js
@@ -1,1 +1,10 @@
-const config_url = ""
+const config_url = "../config.json"
+
+
+async function load_config(){
+    response = await fetch(config_url)
+    
+    console.log(response.json())
+}
+
+load_config()
